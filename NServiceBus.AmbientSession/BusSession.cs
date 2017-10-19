9d026876b0c0834cbf657fbf281fe15558ff067d
Tim <bussmann.t@gmail.com>
2017-10-19 09:18:12 +0200

    add safeguards to prevent overwriting existing contexts

--- NServiceBus.AmbientSession/BusSession.cs
+++ NServiceBus.AmbientSession/BusSession.cs
@@ -1,24 +1,32 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 namespace NServiceBus.AmbientSession
 {
     public static class BusSession
     {
-        private static IBusSession MessageSession = null; 
+        private static IBusSession MessageSession = null;
         private static readonly AsyncLocal<IBusSession> PipelineContext = new AsyncLocal<IBusSession>();
 
-        public static IBusSession Current
-        {
-            get => PipelineContext.Value ?? MessageSession;
-        }
+        public static IBusSession Current => PipelineContext.Value ?? MessageSession;
 
         internal static void SetCurrentPipelineContext(IPipelineContext session)
         {
+            if (PipelineContext.Value != null)
+            {
+                throw new InvalidOperationException("Attempt to overwrite an existing pipeline context in BusSession.Current.");
+            }
+
             PipelineContext.Value = new PipelineContextSession(session);
         }
 
         internal static void SetMessageSession(IMessageSession messageSession)
         {
+            if (MessageSession != null)
+            {
+                throw new InvalidOperationException("Attempt to overwrite an existing message session in BusSession.Current.");
+            }
+
             MessageSession = new MessageSession(messageSession);
         }
     }
