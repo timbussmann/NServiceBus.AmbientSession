e6659559e06b8b4d72dff42b1acf3c3b3dc9afa5
Tim <bussmann.t@gmail.com>
2017-10-20 11:55:49 +0200

    autoregister bus session in container

---
+++ src/NServiceBus.AmbientSession/CurrentSessionResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NServiceBus.AmbientSession
+{
+    class CurrentSessionResolver : IBusSession
+    {
+        public Task Send(object message, SendOptions options)
+        {
+            return BusSession.Current.Send(message, options);
+        }
+
+        public Task Send<T>(Action<T> messageConstructor, SendOptions options)
+        {
+            return BusSession.Current.Send(messageConstructor, options);
+        }
+
+        public Task Publish(object message, PublishOptions options)
+        {
+            return BusSession.Current.Publish(message, options);
+        }
+
+        public Task Publish<T>(Action<T> messageConstructor, PublishOptions publishOptions)
+        {
+            return BusSession.Current.Publish(messageConstructor, publishOptions);
+        }
+    }
+}
